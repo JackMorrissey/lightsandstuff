34a79905b01603db5adff99936ff32d61850c340
JackMorrissey <code@jmmorrissey.com>
2015-06-13 13:05:26 -0500

    feat(configuration): allow environment variable configuration

--- configuration/index.js
+++ configuration/index.js
@@ -1,11 +1,19 @@
 module.exports = getConfiguration;
 
 function getConfiguration(key){
+    var envVariable = process.env[key];
+    if(envVariable){
+        return envVariable;
+    }
+
     var secrets;
     try {
         secrets = require('./secrets');
     } catch(e){
-        throw new Error('Unable to load the configuration. You need to add a secrets file');
+        throw new Error('Unable to load the configuration. You need to add an env variable or a secrets file');
+    }
+    if(!secrets[key]){
+        throw new Error('Unable to find "' + key + '" in the secrets file or env variables');
     }
     return secrets[key];
 }
